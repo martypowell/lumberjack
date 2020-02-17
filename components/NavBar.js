3bda8a02a4466b9fe09744aa16a5d6ba23934655
Marty Powell <jpowel25@gmail.com>
2020-02-17 14:19:41 -0500

    basic navbar copmonent

---
+++ components/NavBar.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const Navbar = props => (
+  <nav>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
