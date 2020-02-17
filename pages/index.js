99a16367b76f3cbeedf1e4d98431b404817c4eee
Marty Powell <jpowel25@gmail.com>
2020-02-17 14:49:00 -0500

    added styling to components

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,27 @@ import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 
 const LogLink = ({ id, label }) => (
-  <Link href="/logs/[id]" as={`logs/${id}`}>
-    <a>{label}</a>
-  </Link>
+  <>
+    <Link href="/logs/[id]" as={`logs/${id}`}>
+      <a>{label}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </>
 );
 
 const LogItem = ({ id, type, message, date }) => {
@@ -31,10 +49,35 @@ const LogItem = ({ id, type, message, date }) => {
 
 const Index = ({ logs, ...rest }) => (
   <DefaultLayout>
+    <h1>LumberJack</h1>
     <h2>Most Recent Logs</h2>
     {logs.map(log => (
       <LogItem key={log.id} {...log} />
     ))}
+    <style jsx>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </DefaultLayout>
 );
 
