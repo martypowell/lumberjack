d25a6bc691ebe44fe5e74493b7328ef1df1258a7
Marty Powell <jpowel25@gmail.com>
2020-02-18 20:01:18 -0500

    added missing id

--- pages/api/logs.js
+++ pages/api/logs.js
@@ -1,20 +1,24 @@
 const logs = [
   {
+    id: 1,
     type: "warning",
     message: "This is a sample warning message.",
     date: "2019-07-04T13:33:03.969Z"
   },
   {
+    id: 2,
     type: "error",
     message: "Something went terribly wrong.",
     date: "2019-07-04T13:33:03.969Z"
   },
   {
+    id: 3,
     type: "info",
     message: "The sky is blue.",
     date: "2019-07-04T13:33:03.969Z"
   },
   {
+    id: 4,
     type: "custom",
     message: "This is a custom message.",
     date: "2019-07-04T13:33:03.969Z"
