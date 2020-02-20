40737a1a91a6d272732453aaae368bac2a9483e1
Marty Powell <jpowel25@gmail.com>
2020-02-19 21:25:22 -0500

    sahred data across logs api

--- pages/api/logs/index.js
+++ pages/api/logs/index.js
@@ -1,29 +1,4 @@
-let logs = [
-  {
-    id: 1,
-    type: "warning",
-    message: "This is a sample warning message.",
-    date: "2019-07-04T13:33:03.969Z"
-  },
-  {
-    id: 2,
-    type: "error",
-    message: "Something went terribly wrong.",
-    date: "2019-07-04T13:33:03.969Z"
-  },
-  {
-    id: 3,
-    type: "info",
-    message: "The sky is blue.",
-    date: "2019-07-04T13:33:03.969Z"
-  },
-  {
-    id: 4,
-    type: "custom",
-    message: "This is a custom message.",
-    date: "2019-07-04T13:33:03.969Z"
-  }
-];
+import Logs from "../../../data/Logs";
 
 /**
  * Handle GET requests to the logs endpoint
@@ -31,7 +6,7 @@ let logs = [
  * @param {object} res
  */
 const handleGET = (req, res) => {
-  res.status(200).json(logs);
+  res.status(200).json(Logs);
 };
 
 /**
