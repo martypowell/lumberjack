065a705304e27c5c81a7a5594b58b40f8090bbbe
Marty Powell <jpowel25@gmail.com>
2020-03-29 09:44:16 -0400

    started adding authtneticaiton

---
+++ common/Authentication.js
@@ -0,0 +1,15 @@
+import { initAuth0 } from "@auth0/nextjs-auth0";
+
+export default initAuth0({
+  domain: "lumberjack.auth0.com",
+  clientId: "1Q1R4fDYhCpB4lTNEw5U2vM8Lbp1lPhd",
+  clientSecret:
+    "ao2_3OriogcpDXWiAz0byL6UZR5l-4zrkMGk4SovIyNuSlpmIlTcfDUndiguGra4",
+  scope: "openid profile",
+  redirectUri: "http://localhost:3000/api/callback",
+  postLogoutRedirectUri: "http://localhost:3000/",
+  session: {
+    cookieSecret: "some-very-very-very-very-very-very-very-very-long-secret",
+    cookieLifetime: 60 * 60 * 8
+  }
+});
--- pages/login.js
+++ pages/login.js
@@ -2,10 +2,32 @@ import DefaultLayout from "../components/DefaultLayout";
 
 const Login = props => (
   <DefaultLayout>
+<<<<<<< Updated upstream
     <form>
       <p>form goes here</p>
     </form>
   </DefaultLayout>
 );
 
+=======
+    <form>form goes here</form>
+  </DefaultLayout>
+);
+
+Login.getInitialProps = async ({ req, res }) => {
+  if (typeof window === "undefined") {
+    const { user } = await auth0.getSession(req);
+    if (!user) {
+      res.writeHead(302, {
+        Location: "/api/login"
+      });
+      res.end();
+      return;
+    }
+
+    return { user };
+  }
+};
+
+>>>>>>> Stashed changes
 export default Login;
